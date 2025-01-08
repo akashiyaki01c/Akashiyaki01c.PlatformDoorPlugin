7976a2c9c23759eb74091c854bf820a2b7b7053e
akashiyaki01c <tanakahiroki.kobe@gmail.com>
2025-01-08 23:00:36 +0900

    delete: デバッグ用のシェルスクリプトを削除

--- run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-msbuild -p:Configuration=Release
-cp ./bin/Release/Akashiyaki01c.PlatformDoorPlugin.dll ./TestScenario/Basic/Akashiyaki01c.PlatformDoorPlugin.dll
-wine ~/Winapp/Bve5.8/BveTs.exe ./TestScenario/scenario.ini
